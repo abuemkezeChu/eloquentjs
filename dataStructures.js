913bbdb0edd4da8c34ce24cefcc9c394599c92d5
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-28 11:07:53 +0100

    working with objects

--- dataStructures.js
+++ dataStructures.js
@@ -1,7 +1,31 @@
-'use strict';
+'use strict'
 
-// methods 
+// methods
 
-let doh = 'DOH'
-console.log(typeof doh.toUpperCase);
-console.log(doh.toUpperCase());
+const doh = 'DOH'
+console.log(typeof doh.toUpperCase)
+console.log(doh.toUpperCase())
+
+// objects
+
+// creating an object
+const day1 = {
+  squirrel: false,
+  events: ['work', 'touched tree', 'pizza', 'running'],
+  'touched tree': 'Touched Tree',
+  age: 20
+}
+
+// playing with methods and properties available on objects
+let anObject = {
+  left: 1,
+  right: 2,
+  center: 0.5
+}
+
+console.log(anObject.left);
+delete anObject.left
+console.log(anObject);
+console.log("left" in anObject);
+
+console.log(Object.keys(anObject));
