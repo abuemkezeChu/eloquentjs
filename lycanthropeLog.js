ef9b73e8561148656a28b75fc9613131ce02358f
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-29 11:46:44 +0100

    set methods

--- lycanthropeLog.js
+++ lycanthropeLog.js
@@ -6,7 +6,28 @@ const addEntry = (events, squirrel) => {
   journal.push({ events, squirrel })
 }
 
-
 addEntry(['work', 'touched tree', 'pizza', 'running', 'television'], false)
-addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree", "brushed teeth"], false);
-addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true);
+addEntry(['work', 'ice cream', 'cauliflower', 'lasagna', 'touched tree', 'brushed teeth'], false)
+addEntry(['weekend', 'cycling', 'break', 'peanuts', 'beer'], true)
+
+/**
+ * computing correlation
+ *ϕ =	n11n00 − n10n01 / √ n1•n0•n•1n•0
+ */
+const phi = (table) => {
+  return (table[3] * table[0] - table[2] * table[1]) / Math.sqrt((table[2] + table[3]) * (table[0] + table[1]) * (table[1] + table[3]) * (table[0] + table[2]))
+}
+
+// Extract the events from the journal array
+const extract = (event, journal) => {
+  const table = [0, 0, 0, 0]
+  for (let i = 0; i < journal.length; i++) {
+    const entry = journal[i]; let index = 0
+    if (entry.events.includes(event)) index++
+    if (entry.squirrel) index += 2
+    table[index]++
+  }
+  return table
+}
+
+console.log(extract('pizza', journal))
--- sets.js
+++ sets.js
@@ -1,4 +1,15 @@
 const array = [1, 2, 3, 4, 5]
 const set = new Set(array)
 
+console.log('Adding elements to a set');
+console.log(set);
+set.add(['chu', 'vicky', 'paula'])
+console.log(set);
 
+console.log('Deleting elements in a set');
+set.delete(3)
+console.log(set);
+
+console.log('Clearing a set');
+set.clear()
+console.log(set);
