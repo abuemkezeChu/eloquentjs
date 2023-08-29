e78f001936bd6318b3b5fb77be6571491a700adb
Abuemkeze Chu <abuemkeze@gmail.com>
2023-08-29 12:06:44 +0100

    working with maps

---
+++ maps.js
@@ -0,0 +1,12 @@
+// console.log(myMap);
+// const myObject = {}
+
+
+const a = {
+  name: 'Chu'
+}
+const b = {}
+
+const myMap = new Map([[a, 'firstObject'], [b, 'secondObject']])
+
+console.log(myMap);
